de3b0e6ded6d26b07c93a32878ad85bcdf9f71ce
hyounji375 <hyounji375@gmail.com>
2022-07-28 00:31:43 +0900

    0727

--- src/components/add.js
+++ src/components/add.js
@@ -1,15 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Add = ({ onClickEvent, stateNum }) => {
   const [name, setName] = useState("");
+  const nameInput = useRef(null);
 
   const onChangeInput = (e) => {
     setName(e.target.value);
     console.log(name);
   };
+
   const RunonClickEvent = () => {
     onClickEvent(stateNum + 1, name);
     setName("");
+  };
+
+  const Reset = () => {
+    setName("");
+    nameInput.current.focus();
+    console.log(name);
+    console.log(nameInput);
+  };
+
+  const AddEnter = (e) => {
+    console.log(e);
+    if (e.key === "Enter") {
+      onClickEvent(stateNum + 1, name);
+      setName("");
+      alert("값이 추가되었습니다.");
+    }
   };
   return (
     <>
@@ -18,9 +36,18 @@ const Add = ({ onClickEvent, stateNum }) => {
         placeholder="나라 이름을 입력해주세요"
         value={name}
         onChange={onChangeInput}
+        onKeyPress={AddEnter}
+        ref={nameInput}
       />
-      <button onClick={RunonClickEvent}>추가</button>
-      <button>초기화</button>
+      <button
+        onClick={() => {
+          RunonClickEvent();
+          Reset();
+        }}
+      >
+        추가
+      </button>
+      <button onClick={Reset}>초기화</button>
     </>
   );
 };
--- src/components/map.js
+++ src/components/map.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Add from "./add";
 
 const Map = () => {
@@ -12,12 +12,37 @@ const Map = () => {
   const onClickEvent = (numValue, nameValue) => {
     setCountry([...country, { number: numValue, name: nameValue }]);
   };
+
+  const Remove = (e) => {
+    const RemoveCountry = country.filter(
+      (c) => c.number !== parseInt(e.target.value)
+    );
+    console.log(e.target.value);
+    setCountry(RemoveCountry);
+  };
+
+  const RemoveArr = useRef([]);
+  const Remove_2 = () => {
+    console.log(RemoveArr);
+  };
+
+  const Remove_3 = (cNum) => {
+    console.log(cNum);
+    const remove3 = country.filter((c) => c.number !== cNum);
+    setCountry(remove3);
+  };
   return (
     <>
-      {country.map((c) => (
+      {country.map((c, i) => (
         <div>
           {c.number}번은 {c.name}입니다.
-          <button>제거</button>
+          <button
+            onClick={() => Remove_3(c.number)}
+            value={c.number}
+            ref={(el) => (RemoveArr[i] = el)}
+          >
+            삭제
+          </button>
         </div>
       ))}
       <Add
